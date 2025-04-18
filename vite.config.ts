3d191f602415dca2b5dfebb62593553b486c15c7
elizaeee <103398358+elizaeee@users.noreply.github.com>
2025-04-17 22:23:52 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Ice-breaking-game/', // 💥 Required for GitHub Pages
   plugins: [react()],
   server: {
     port: 5173,
     open: true
   }
-}); 
+});
